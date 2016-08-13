6f8955f5c9cc4f5ea5138608280e34657298a87d
wangweiyan0901 <305470124@qq.com>
2016-08-13 18:20:08 +0800

    新建项目

---
+++ baisi_wangweiyan/baisi_wangweiyan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baisi_wangweiyan
+//
+//  Created by 汪炜焱 on 16/8/13.
+//  Copyright © 2016年 wangeweiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ baisi_wangweiyan/baisi_wangweiyan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  baisi_wangweiyan
+//
+//  Created by 汪炜焱 on 16/8/13.
+//  Copyright © 2016年 wangeweiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
